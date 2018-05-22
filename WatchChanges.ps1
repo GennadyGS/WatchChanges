d97ad2e987fb973bbd5e2286b0b183ae9dda356d
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-05-22 18:36:22 +0300

    Initial changes

---
+++ WatchChanges.ps1
@@ -0,0 +1,19 @@
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = Get-Location
+$watcher.IncludeSubdirectories = $true
+$watcher.EnableRaisingEvents = $false
+$watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite -bor [System.IO.NotifyFilters]::FileName
+
+while($true){
+    "Watching for changes in $(Get-Location)..."
+    $result = $watcher.WaitForChanged([System.IO.WatcherChangeTypes]::Changed `
+        -bor [System.IO.WatcherChangeTypes]::Renamed `
+        -bOr [System.IO.WatcherChangeTypes]::Created, 1000);
+	if($result.TimedOut) {
+		continue;
+	}
+    Write-Host "Change in " + $result.Name
+    if ($args) {
+        & $args[0] $args[1..$($args.Count - 1)]
+    }
+}
